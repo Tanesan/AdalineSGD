6407918aeb9ed1dc76eb5e7c8e9746446d9bb95e
Keito <54165015+Tanesan@users.noreply.github.com>
2021-06-13 21:07:21 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,137 @@
+import numpy as np
+from matplotlib.colors import ListedColormap
+
+
+class AdalineSGD(object):
+    # eta: float  # 学習率　0-1
+    # n_iter: int  # training data 訓練データの訓練回数
+    # random_state: int  # 重みを初期化するための乱数シード
+    # shuffle: bool (True) #試行ごとに訓練データをシャッフル
+
+    # classに値追加
+    def __init__(self, eta=0.01, n_iter=500, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        self.random_state = random_state
+
+    def partial_fit(self, X, y):
+        # おもみを再初期化しない
+        if not self.w_initialized:  # 初期化されていないと初期化する
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:  # 要素数が２以上だと重み更新
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+    def fit(self, X, y):
+        # 重みベクトル作成
+        self._initialize_weights(X.shape[1])  # .. 要素数　（この場合は要素２つ）
+        self.cost_ = []
+        for i in range(self.n_iter):  # 訓練回数
+            if self.shuffle:  # 訓練データをシャッフルする場合はtrueになりシャッフルする。for文内なので一回一回実行される
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))  # 重みを更新
+            avg_cost = sum(cost) / len(y)  # 訓練データの平均コストの計算
+            self.cost_.append(avg_cost)  # 平均コストを格納
+        return self
+
+    def _shuffle(self, X, y):
+        r = self.rgen.permutation(len(y))  # numpyのランダム関数に加え、それをrに代入という関数 permutation
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        self.rgen = np.random.RandomState(
+            self.random_state)  # 乱数の再現性を担保　randomstateは乱数調整表のように、ランダムではあるが何回実行しても同じ値を示す。seedと同意
+        self.w_ = self.rgen.normal(loc=0.0, scale=0.01, size=1 + m)  # 正規分布を使った乱数調整　重みベクトルを作成
+        # X.shape = [100 ,2] 100-訓練データの数, 2-特徴量の数（2要素）
+        # loc Avr, scale 標準偏差　size 出力配列のサイズ
+        self.w_initialized = True  # True
+
+    def _update_weights(self, xi, target):
+        output = self.activation(self.net_input(xi))  # activation関数は今は空っぽなので、output = net_inputでも可
+        error = (target - output)  # 誤差
+        # yが実際の値　outputは結果　残差平方和なので、残差(誤差)を求める
+        self.w_[1:] += self.eta * xi.dot(error)  # ここは転地しない？
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error ** 2
+        return cost
+
+    def net_input(self, X):
+        # n.dot ベクトルの内積
+        # 実際の要素Xとランダムに作成された数値の内積を取り、それにランダムで作成した値をたす。それが0以上であれば1...
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        return X
+
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=cl,
+                    edgecolor='black')
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+s = os.path.join('https://archive.ics.uci.edu', 'ml', 'machine-learning-databases', 'iris', 'iris.data')
+df = pd.read_csv(s, header=None, encoding='utf-8')
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+X = df.iloc[0:100, [0, 2]].values
+# plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+# plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+
+# 1行2列に変換
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+# ADALINEを実装する前に、データを標準化してみる
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+# ADALINE実装　訓練回数10 学習率0.01
+ada1 = AdalineSGD(n_iter=500, eta=0.1)
+ada1.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada1)
+plt.title("adaline - Gradient Descent")
+plt.xlabel('sepal length [std]')
+plt.ylabel('petal length [std]')
+
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+ada1.partial_fit(X_std[0, :], y[0])
